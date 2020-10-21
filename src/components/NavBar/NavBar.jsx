952d9de2f7f8049089a8c2968f4e2fc45f9dbcb5
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-21 18:56:07 -0400

    edit navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,10 +6,10 @@ import { Link} from "react-scroll";
 const NavBar = (props) => {
     return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">
+                {/* <a className="navbar-brand" href="/">
                     <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <a className="navbar-brand" href="/">Gabriela Cabrera</a>
+                </a> */}
+                <a className="navbar-brand" href="/">&nbsp;Gabriela Cabrera</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
